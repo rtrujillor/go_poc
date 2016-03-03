d306da14c285e94c8395a08b56e376abbf59ae97
Ricardo Trujillo Rodríguez <rtrujillor@hotmail.com>
2016-03-03 12:51:35 +0100

    Update hello.go
    Added commentaries

--- hello.go
+++ hello.go
@@ -1,7 +1,9 @@
+// Every Go source file starts with a package clause, like this:
 package main
-
+// To use a package, the importing source file identifies it by its package path in the import clause
 import "fmt"
 
 func main() {
+	// Then the package name (as distinct from path) is used to qualify items from the package in the importing source file
 	fmt.Printf("Hello, world.\n")
 }
